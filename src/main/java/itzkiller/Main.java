b9afaf19feeb6ddb652874ff45a798b0769e8623
ITZKILLER <raiabhijit711@gmail.com>
2024-09-20 02:29:50 +0530

    added hub

--- src/main/java/itzkiller/Main.java
+++ src/main/java/itzkiller/Main.java
@@ -2,6 +2,7 @@ package itzkiller;
 
 
 import net.minestom.server.MinecraftServer;
+
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
@@ -11,7 +12,7 @@ import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 
 import net.minestom.server.instance.LightingChunk;
-import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.anvil.AnvilLoader;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,14 +27,14 @@ public class Main {
         InstanceContainer instanceContainer=instanceManager.createInstanceContainer();
 
         //chunks
-        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0,40, Block.GRASS_BLOCK));
+        instanceContainer.setChunkLoader(new AnvilLoader("resources/worlds/hub"));
 
         //events handler
         GlobalEventHandler globalEventHandler=MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             final Player player=event.getPlayer();
             event.setSpawningInstance(instanceContainer);
-            player.setRespawnPoint(new Pos(0,42,0));
+            player.setRespawnPoint(new Pos(-3,70,-68));
         });
 
         //lighting
